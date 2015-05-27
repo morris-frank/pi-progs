66a01c87691dbd42fa2f9c33fc0db7037d30cc3c
Tukkin Moriarty <mrtukkin@gmail.com>
2015-05-28 01:07:33 +0200

    updt script

--- carousel.c
+++ carousel.c
@@ -11,13 +11,17 @@ int main(void)
 	pinMode(22, OUTPUT);
 	for(;;)
 	{
-		for(pin = 0; pin < 23; pin++)
+		for(pin = 0; pin < 8; pin++)
 		{
-			printf("%d\n", pin);
 			digitalWrite(pin, LOW);
-			if(pin == 7)
-				pin = 20;
-			digitalWrite((pin+3)%23, HIGH);
+			digitalWrite((pin+3)%8, HIGH);
+			if(pin % 2 == 0){
+				digitalWrite(21, HIGH);
+				digitalWrite(22, LOW);
+			}else{
+				digitalWrite(21, LOW);
+				digitalWrite(22, HIGH);
+			}
 			delay(50);
 		}
 	}
