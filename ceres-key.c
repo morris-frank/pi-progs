b57bb0fe0b3b0567dee7fe2d8869031cfdae6560
Tukkin Moriarty <mrtukkin@gmail.com>
2015-06-10 11:48:32 +0200

    backup

--- ceres-key.c
+++ ceres-key.c
@@ -6,6 +6,11 @@ int rand_int(int max){
     return 0;
 }
 
+void print_times(char * string, int x){
+    for(x; x > 0; x--)
+        printf("%s", string);
+}
+
 typedef int KeyType;
 
 struct SentenceList{
@@ -23,16 +28,16 @@ struct KeyList{
     struct KeyListItem * first;
 };
 
-struct Node{
+struct TrieNode{
     KeyType key;
-    struct Node * sibling;
-    struct Node * child;
+    struct TrieNode * sibling;
+    struct TrieNode * child;
     unsigned int hasSentences : 1;
     struct SentenceList * sentences;
 };
 
 struct Trie{
-    struct Node * root;
+    struct TrieNode * root;
 };
 
 void init_sentencelist(struct SentenceList * list){
@@ -40,7 +45,7 @@ void init_sentencelist(struct SentenceList * list){
     list->list = (char ***)malloc(0 * sizeof(char **));
 }
 
-void init_key(struct KeyListItem * key){
+void init_keylistitem(struct KeyListItem * key){
     key->key = 0;
     key->next = 0;
 }
@@ -48,6 +53,18 @@ void init_key(struct KeyListItem * key){
 void init_keylist(struct KeyList * list){
     list->count = 0;
     list->first = 0;
+}
+
+void init_trienode(struct TrieNode * node){
+    node->key = 0;
+    node->child = 0;
+    node->sibling = 0;
+    node->hasSentences = 0;
+    node->sentences = 0;
+}
+
+void init_trie(struct Trie * trie){
+    trie->root = 0;
 }
 
 void append_sentence(struct SentenceList * list, char * sentence){
@@ -63,31 +80,125 @@ void malloc_sentences(struct SentenceList * list, char * sentence){
     append_sentence(list, sentence);
 }
 
-void init_node(struct Node * node){
-    node->key = 0;
-    node->child = 0;
-    node->sibling = 0;
-    node->hasSentences = 0;
-    node->sentences = 0;
-}
-
-void init_trie(struct Trie * trie){
-    trie->root = 0;
-}
-
 void sort_keylist(struct KeyList * list){
     return;
 }
 
-void print_times(char * string, int x)
+struct TrieNode * search_sentence_in_trie(struct Trie * trie, struct KeyList * keylist)
 {
-    for(x; x > 0; x--)
-        printf("%s", string);
+    struct TrieNode * trie_iterator = trie->root;
+    struct TrieNode * last_sentenced = 0;
+    struct KeyListItem * keylist_iterator = keylist->first;
+
+    while(keylist_iterator != 0){
+
+        if( keylist_iterator->key == trie_iterator->key )
+            if(trie_iterator->child != 0)
+                trie_iterator = trie_iterator->child;
+            else
+                keylist_iterator = 0;
+
+        else
+            if(trie_iterator->sibling != 0)
+                trie_iterator = trie_iterator->sibling;
+            else
+                trie_iterator = last_sentenced->sibling;
+
+        if(trie_iterator->hasSentences = 1)
+            last_sentenced = trie_iterator;
+
+        keylist_iterator = keylist_iterator->next;
+    }
+
+    return last_sentenced;
 }
 
-void print_trie_recursive(struct Node * root, int prefix)
+void sentence_into_trie(struct Trie * trie, struct KeyList * keylist, char * sentence)
 {
-    struct Node * trie_iterator = root;
+    struct TrieNode * trie_iterator = trie->root;
+    struct TrieNode * trie_iterator_prec = trie_iterator;
+    struct KeyListItem * keylist_iterator = keylist->first;
+
+    //Trie ist leer:
+    if(trie_iterator == 0)
+    {
+        trie_iterator = (struct TrieNode *)malloc(sizeof(struct TrieNode));
+        init_trienode(trie_iterator);
+        trie_iterator->key = keylist_iterator->key;
+        keylist_iterator = keylist_iterator->next;
+        trie->root = trie_iterator;
+        trie_iterator_prec = trie_iterator;
+
+    //Trie ist nicht leer
+    }else{
+        //Solange nicht an einem Trie Ende oder Listen Ende
+        while( trie_iterator !=0 && keylist_iterator != 0){
+
+            //hier ist trie_iterator == trie_iterator_prec erfüllt sein
+            //Node passt, auf zum Kind
+            if(trie_iterator->key == keylist_iterator->key){
+
+                trie_iterator = trie_iterator->sibling;
+
+                while( trie_iterator != 0 && trie_iterator->key < keylist_iterator->key ){
+                    trie_iterator = trie_iterator->sibling;
+                    trie_iterator_prec = trie_iterator_prec->sibling;
+                }
+
+                //Neuer Sibling am Ende nötig
+                if(trie_iterator == 0){
+                    trie_iterator_prec->sibling = (struct TrieNode *)malloc(sizeof(struct TrieNode));
+                    trie_iterator = trie_iterator_prec->sibling;
+                    trie_iterator_prec = trie_iterator;
+                    init_trienode(trie_iterator);
+                    trie_iterator->key = keylist_iterator->key;
+                    keylist_iterator = keylist_iterator->next;
+                    break;
+
+                //Passender Sibling gefunden
+                }else if(trie_iterator->key == keylist_iterator->key){
+                    trie_iterator = trie_iterator->child;
+                    trie_iterator_prec = trie_iterator;
+                    keylist_iterator = keylist_iterator->next;
+
+                //Neuer Sibling in Mitte
+                }else if(trie_iterator->key < keylist_iterator->key){
+                    trie_iterator_prec->sibling = (struct TrieNode *)malloc(sizeof(struct TrieNode));
+                    trie_iterator_prec->sibling->sibling = trie_iterator;
+                    trie_iterator = trie_iterator_prec->sibling;
+                    trie_iterator_prec = trie_iterator;
+                    init_trienode(trie_iterator);
+                    trie_iterator->key = keylist_iterator->key;
+                    keylist_iterator = keylist_iterator->next;
+                }
+            //Node passt nicht, auf zum Sibling
+            }else{
+                trie_iterator = trie_iterator->child;
+                trie_iterator_ prec = trie_iterator;
+                keylist_iterator = keylist_iterator->next;
+            }
+        }
+    }
+
+    while(keylist_iterator !=0 ){
+        trie_iterator->child = (struct TrieNode *)malloc(sizeof(struct TrieNode));
+        trie_iterator = trie_iterator->child;
+        init_trienode(trie_iterator);
+        trie_iterator->key = keylist_iterator->key;
+        keylist_iterator = keylist_iterator->next;
+    }
+
+    if(trie_iterator->hasSentences == 0){
+        trie_iterator->hasSentences = 1;
+        malloc_sentences(trie_iterator->sentences, sentence);
+    }else{
+        append_sentence(trie_iterator->sentences, sentence);
+    }
+}
+
+void print_trie_recursive(struct TrieNode * root, int prefix)
+{
+    struct TrieNode * trie_iterator = root;
     print_times(" ", prefix);
     printf(" | \n", prefix);
     while(trie_iterator != 0)
@@ -116,99 +227,7 @@ void print_trie(struct Trie * trie)
     print_trie_recursive(trie->root, 0);
 }
 
-struct Node * search_sentence_in_trie(struct Trie * trie, struct KeyList * keylist)
-{
-    struct Node * trie_iterator = trie->root;
-    struct Node * last_sentenced = 0;
-    struct KeyListItem * keylist_iterator = keylist->first;
-    while(keylist_iterator != 0){
-        if( keylist_iterator->key == trie_iterator->key ){
-            if(trie_iterator->child != 0){
-                trie_iterator = trie_iterator->child;
-            }else{
-                keylist_iterator = 0;
-            }
-        }else{
-            if(trie_iterator->sibling != 0){
-                trie_iterator = trie_iterator->sibling;
-            }else{
-                //While parent ----->>>
-                trie_iterator = last_sentenced->sibling;
-            }
-        }
-        if(trie_iterator->hasSentences = 1){
-            last_sentenced = trie_iterator;
-        }
-        keylist_iterator = keylist_iterator->next;
-    }
-    return last_sentenced;
-}
-
-void sentence_into_trie(struct Trie * trie, struct KeyList * keylist, char * sentence)
-{
-    struct Node * trie_iterator = trie->root;
-    struct Node * trie_iterator_prec = trie_iterator;
-    struct KeyListItem * keylist_iterator = keylist->first;
-
-
-    if(trie_iterator == 0)
-    {
-        trie_iterator = (struct Node *)malloc(sizeof(struct Node));
-        init_node(trie_iterator);
-        trie_iterator->key = keylist_iterator->key;
-        keylist_iterator = keylist_iterator->next;
-        trie->root = trie_iterator;
-    }else{
-        while( trie_iterator !=0 && keylist_iterator != 0){
-            if( trie_iterator->key != keylist_iterator->key ){
-
-                trie_iterator = trie_iterator->sibling;
-
-                while( trie_iterator != 0 && trie_iterator->key < keylist_iterator->key ){
-                    trie_iterator = trie_iterator->sibling;
-                    trie_iterator_prec = trie_iterator_prec->sibling;
-                }
-
-                if( trie_iterator != 0 && trie_iterator->key == keylist_iterator->key ){
-                    trie_iterator_prec = trie_iterator;
-                    trie_iterator = trie_iterator->child;
-                    keylist_iterator = keylist_iterator->next;
-                }else{
-                    printf("insert sib with %d\n", keylist_iterator->key);
-                    trie_iterator_prec->sibling = (struct Node *)malloc(sizeof(struct Node));
-                    trie_iterator = trie_iterator_prec->sibling;
-                    init_node(trie_iterator);
-                    trie_iterator->key = keylist_iterator->key;
-                    keylist_iterator = keylist_iterator->next;
-                    break;
-                }
-
-            }else{
-                trie_iterator_prec = trie_iterator;
-                trie_iterator = trie_iterator->child;
-                keylist_iterator = keylist_iterator->next;
-            }
-        }
-        trie_iterator = trie_iterator_prec;
-    }
-
-    while(keylist_iterator !=0 ){
-        trie_iterator->child = (struct Node *)malloc(sizeof(struct Node));
-        trie_iterator = trie_iterator->child;
-        init_node(trie_iterator);
-        trie_iterator->key = keylist_iterator->key;
-        keylist_iterator = keylist_iterator->next;
-    }
-
-    if(trie_iterator->hasSentences == 0){
-        trie_iterator->hasSentences = 1;
-        malloc_sentences(trie_iterator->sentences, sentence);
-    }else{
-        append_sentence(trie_iterator->sentences, sentence);
-    }
-}
-
-struct Node * parse_file_to_trie(char * filename)
+struct TrieNode * parse_file_to_trie(char * filename)
 {
 }
 
@@ -225,7 +244,7 @@ int main(){
     struct KeyList keylist_1;
     init_keylist(&keylist_1);
     struct KeyListItem key_1_1, key_1_2;
-    init_key(&key_1_1); init_key(&key_1_2);
+    init_keylistitem(&key_1_1); init_keylistitem(&key_1_2);
     key_1_1.key = 1; key_1_2.key = 3;
     keylist_1.first = &key_1_1; key_1_1.next = &key_1_2;
     keylist_1.count = 2;
@@ -234,7 +253,7 @@ int main(){
     struct KeyList keylist_2;
     init_keylist(&keylist_2);
     struct KeyListItem key_2_1, key_2_2, key_2_3;
-    init_key(&key_2_1); init_key(&key_2_2); init_key(&key_2_3);
+    init_keylistitem(&key_2_1); init_keylistitem(&key_2_2); init_keylistitem(&key_2_3);
     key_2_1.key = 1; key_2_2.key = 3; key_2_1.key = 4;
     keylist_2.first = &key_2_1; key_2_1.next = &key_2_2; key_2_2.next = &key_2_3;
     keylist_2.count = 3;
@@ -243,7 +262,7 @@ int main(){
     struct KeyList keylist_3;
     init_keylist(&keylist_3);
     struct KeyListItem key_3_1, key_3_2;
-    init_key(&key_3_1); init_key(&key_3_2);
+    init_keylistitem(&key_3_1); init_keylistitem(&key_3_2);
     key_3_1.key = 3; key_3_2.key = 5;
     keylist_3.first = &key_3_1; key_3_1.next = &key_3_2;
     keylist_3.count = 2;
